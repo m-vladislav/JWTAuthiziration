2842eef8e3ba90dbd6741c979830464ff7e8577d
Vladislav <vladis13777@gmail.com>
2023-05-25 00:03:01 +0300

    Created router

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import router from './router';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use('/api', router);
 
 const start = async () =>  {
     try {
